c122d6e59e4c8c331e8dd46ce6297c64e560100e
jonathann000 <97891351+jonathann000@users.noreply.github.com>
2022-11-25 11:18:53 +0100

    addedanothertest

--- Test/WorkScheduleTest.java
+++ Test/WorkScheduleTest.java
@@ -1,15 +1,17 @@
 import org.junit.jupiter.api.Test;
 
+import java.sql.SQLOutput;
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class WorkScheduleTest {
 
 
-
     @Test
-    void setRequiredNumber() {
+        // startTime > endTime
+    void setRequiredNumberPartition1() {
         WorkSchedule ws = new WorkSchedule(5);
-
         ws.setRequiredNumber(1, 2, 1);
         int zero = ws.readSchedule(0).requiredNumber;
         int one = ws.readSchedule(1).requiredNumber;
@@ -21,46 +23,73 @@ class WorkScheduleTest {
         assertEquals(0, two);
         assertEquals(0, three);
         assertEquals(0, four);
+    }
 
+    @Test // endTime >= startTime & nemployee < workingEmployee.length[startTime]
+    void setRequiredNumberPartition2() {
+        WorkSchedule ws = new WorkSchedule(3);
 
+        ws.setRequiredNumber(2, 0, 2);
+        ws.addWorkingPeriod("A", 0, 2);
+        ws.addWorkingPeriod("B", 0, 2);
+        ws.setRequiredNumber(1, 0, 2);
+        int reqNum = ws.readSchedule(0).requiredNumber;
 
+        assertEquals(1, ws.readSchedule(0).workingEmployees.length);
+        assertEquals(reqNum, 1);
+        // FOUND BUG ONE: setRequiredNumber does remove all employees from the schedule
     }
 
     @Test
-    void setRequiredNumberBothEqual () {
+        // endTime >= startTime & workingEmployee.length[startTime] < nemployee
+    void setRequiredNumberPartition3() {
+        WorkSchedule ws = new WorkSchedule(3);
 
+        ws.setRequiredNumber(2, 0, 2);
+        ws.addWorkingPeriod("A", 0, 2);
+        ws.addWorkingPeriod("B", 0, 2);
+        ws.setRequiredNumber(3, 0, 2);
+        int reqNum = ws.readSchedule(0).requiredNumber;
+
+        //setRequiredNumber works when the new number is greater than the old number
+        assertEquals(2, ws.readSchedule(0).workingEmployees.length);
+        //requiredNumber is set to the new number
+        assertEquals(reqNum, 3);
+
+
+    }
+    @Test // Endtime is the same length or longer than WorkSchedule.schedule.length
+    void setRequiredNumberPartition4() {
+        WorkSchedule ws = new WorkSchedule(10);
+
+        ws.setRequiredNumber(0, 0, 10);
+        ws.addWorkingPeriod("A", 0, 10);
+
+
+        //setRequiredNumber works when the new number is greater than the old number
+        assertEquals(0, ws.readSchedule(0).workingEmployees.length);
+    }
+
+    @Test
+        // “input: currentTime = 0”, given schedule[0].workingEmployees.length < schedule[0].requiredNumber
+    void nextIncompleteTest1() {
         WorkSchedule ws = new WorkSchedule(5);
-        int beforeSchedule = ws.readSchedule(2).requiredNumber;
-        ws.setRequiredNumber(1,2,2;
-        assertEquals(beforeSchedule, ws.readSchedule(2).requiredNumber);
+        ws.setRequiredNumber(2, 0, 2);
+        ws.addWorkingPeriod("A", 0, 2);
+        int next = ws.nextIncomplete(0);
+        assertEquals(0, next);
+        //FOUND BUG TWO: nextIncomplete does not return the correct index
+        // always does return the last index of the schedule
     }
 
     @Test
-    void setRequiredNumber3() {
-        WorkSchedule ws = new WorkSchedule(2);
+        // “input: currentTime = 0”, given schedule[0].workingEmployees.length == schedule[0].requiredNumber
+    void nextIncompleteTest2() {
+        WorkSchedule ws = new WorkSchedule(3);
+        ws.setRequiredNumber(1, 0, 2);
+        ws.addWorkingPeriod("A", 0, 2);
+        int next = ws.nextIncomplete(0);
+        assertEquals(-1, next);
 
-
-
-
-    }
-
-    @Test
-    void setRequiredNumber4() {
-        WorkSchedule ws = new WorkSchedule(5);
-        ws.setRequiredNumber(5, 1,2);
-        int zero = ws.readSchedule(0).requiredNumber;
-
-        assertEquals(5, zero);
-    }
-
-    @Test
-    void setRequiredNumber5() {
-        WorkSchedule ws = new WorkSchedule(5);
-        ws.setRequiredNumber(0,0,0);
-    }
-
-
-    @Test
-    void nextIncomplete() {
     }
 }
